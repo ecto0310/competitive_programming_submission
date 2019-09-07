b415319dc00cdeec305ab75dcc1c5ab1da53bbbe
ecto0310 <ecto0310@gmail.com>
2019-09-07 15:02:29 +0000

    [add] submission/codeforces/1211/1211_b/60225637.kt

---
+++ codeforces/1211/1211_b/60225637.kt
@@ -0,0 +1,16 @@
+// Date: Sat, 07 Sep 2019 15:02:29 +0000
+// Language: Kotlin 1.4
+fun main(args: Array<String>)
+{
+  val n = readLine()!!.toInt()
+  val a = readLine()!!.split(" ").map(String::toInt)
+  val m = a.max()
+  var it=-1
+  if(m!=null)
+  {
+    for(i in 0..n-1)
+      if(a[i]==m)
+        it=i
+    println(n.toLong()*(m-1)+it+1)
+  }
+}

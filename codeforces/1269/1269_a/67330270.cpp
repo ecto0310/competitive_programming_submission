0aa0821065de8f6931ef51ef70a8bd87e590d015
ecto0310 <ecto0310@gmail.com>
2019-12-21 11:07:42 +0000

    [add] submission/codeforces/1269/1269_a/67330270.cpp

---
+++ codeforces/1269/1269_a/67330270.cpp
@@ -0,0 +1,13 @@
+// Date: Sat, 21 Dec 2019 11:07:42 +0000
+// Language: GNU C++17
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 n;
+  cin >> n;
+  cout << 16 * n << " " << 15 * n << endl;
+  return 0;
+}

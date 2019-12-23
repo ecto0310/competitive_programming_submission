c117327ae9e6647a61e65686929fd142692005d3
ecto0310 <ecto0310@gmail.com>
2019-12-23 07:04:22 +0000

    [add] submission/codeforces/1269/1269_d/67456343.cpp

---
+++ codeforces/1269/1269_d/67456343.cpp
@@ -0,0 +1,28 @@
+// Date: Mon, 23 Dec 2019 07:04:22 +0000
+// Language: GNU C++17
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main()
+{
+  i64 n;
+  cin >> n;
+  vector<i64> a(n);
+  for (i64 i = 0; i < n; i++)
+    cin >> a[i];
+  i64 w = 0, b = 0;
+  for (i64 i = 0; i < n; i++)
+    if (i % 2 == 0)
+    {
+      w += a[i] / 2 + a[i] % 2;
+      b += a[i] / 2;
+    }
+    else
+    {
+      b += a[i] / 2 + a[i] % 2;
+      w += a[i] / 2;
+    }
+  cout << min(w, b) << endl;
+  return 0;
+}

c9d8ad991d377feed06e6351273d947a38138937
ecto0310 <ecto0310@gmail.com>
2020-04-15 14:46:02 +0000

    [add] submission/codeforces/1337/1337_b/76820819.cpp

---
+++ codeforces/1337/1337_b/76820819.cpp
@@ -0,0 +1,28 @@
+// Date: Wed, 15 Apr 2020 14:46:02 +0000
+// Language: GNU C++17
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 t;
+  cin >> t;
+  for (i64 _ = 0; _ < t; _++)
+  {
+    i64 x, n, m;
+    cin >> x >> n >> m;
+    if (x / 2 + 10 < x)
+      for (i64 i = 0; i < n; i++)
+        if (x / 2 + 10 < x)
+          x = x / 2 + 10;
+    for (i64 i = 0; i < m; i++)
+      x -= 10;
+    if (0 < x)
+      cout << "NO" << endl;
+    else
+      cout << "YES" << endl;
+  }
+  return 0;
+}

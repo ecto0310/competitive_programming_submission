05b3cdfd2ce41a763a4a22616f4e939d93f3e132
ecto0310 <ecto0310@gmail.com>
2020-04-15 14:47:49 +0000

    [add] submission/codeforces/1337/1337_a/76823535.cpp

---
+++ codeforces/1337/1337_a/76823535.cpp
@@ -0,0 +1,19 @@
+// Date: Wed, 15 Apr 2020 14:47:49 +0000
+// Language: GNU C++17
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+#define endl "\n"
+
+int main()
+{
+  i64 t;
+  cin >> t;
+  for (i64 _ = 0; _ < t; _++)
+  {
+    i64 a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << b << " " << c << " " << c << endl;
+  }
+  return 0;
+}
